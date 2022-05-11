05a7a2a4786f4f4055d9fb7e20299066912b2d78
Diego Mazorra <ingenierodiegomazorra@gmail.com>
2022-05-11 12:00:31 -0500

    add recipe feature for ingredients and refactor code

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,5 +1,5 @@
 # from django.contrib.auth import get_user_model, authenticate
-from core.models import Tag
+from core.models import Tag, Ingredient
 from rest_framework import serializers
 
 
@@ -8,5 +8,14 @@ class TagSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Tag
+        fields = ('id', 'name')
+        read_only_fields = ('id',)
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    """Serializer for an ingredient object"""
+
+    class Meta:
+        model = Ingredient
         fields = ('id', 'name')
         read_only_fields = ('id',)
